94bbc28b86c3020479de86a213a987fe38a895f0
Nelson <nelson.rs.07@gmail.com>
2020-01-11 14:49:19 -0700

    Removed bootstrap

--- NSBenefits/ClientApp/src/app/app.module.ts
+++ NSBenefits/ClientApp/src/app/app.module.ts
@@ -7,18 +7,18 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
-import { CounterComponent } from './counter/counter.component';
-import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatButtonModule } from '@angular/material';
+import {
+  MatButtonModule,
+  MatToolbarModule
+} from '@angular/material';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     NavMenuComponent,
     HomeComponent,
-    CounterComponent,
-    FetchDataComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
@@ -26,10 +26,9 @@ import { MatButtonModule } from '@angular/material';
     FormsModule,
     BrowserAnimationsModule,
     MatButtonModule,
+    MatToolbarModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
-      { path: 'counter', component: CounterComponent },
-      { path: 'fetch-data', component: FetchDataComponent },
     ])
   ],
   providers: [],
