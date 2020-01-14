f82e5ef5fc62fd5c38f3ca1c6e6c2b4293d10ab3
Nelson <nelson.rs.07@gmail.com>
2020-01-13 20:27:06 -0700

    Got desktop layout working

--- NSBenefits/ClientApp/src/app/cost/chart/chart.component.ts
+++ NSBenefits/ClientApp/src/app/cost/chart/chart.component.ts
@@ -1,16 +1,16 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnChanges } from '@angular/core';
 
 @Component({
   selector: 'app-chart',
   templateUrl: './chart.component.html',
   styleUrls: ['./chart.component.scss']
 })
-export class ChartComponent implements OnInit {
+export class ChartComponent implements OnChanges {
   @Input() salary: number;
   @Input() totalCostPerPayPeriod: number;
-  public costPercentageOfPay: number = 0
+  public costPercentageOfPay: number = 0;
 
-  ngOnInit() {
+  ngOnChanges() {
     this.costPercentageOfPay = ((this.totalCostPerPayPeriod / this.salary) * 100);
   }
 }
