c42693432f2178604b28ac6e000ccc3f1578577f
Nelson <nelson.rs.07@gmail.com>
2020-01-13 20:39:25 -0700

    Made costbar width dynamic

--- NSBenefits/ClientApp/src/app/cost/chart/chart.component.ts
+++ NSBenefits/ClientApp/src/app/cost/chart/chart.component.ts
@@ -9,8 +9,10 @@ export class ChartComponent implements OnChanges {
   @Input() salary: number;
   @Input() totalCostPerPayPeriod: number;
   public costPercentageOfPay: number = 0;
+  public costbarWidth: string;
 
   ngOnChanges() {
     this.costPercentageOfPay = ((this.totalCostPerPayPeriod / this.salary) * 100);
-  }
+    this.costbarWidth = `${this.costPercentageOfPay * 10}px`;
+    }
 }
